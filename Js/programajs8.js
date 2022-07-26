61d58c2a8a04c8cee340df47dd74b435d2f1c9dc
Pato Kuack <nocturno_x_13514@hotmail.com>
2022-07-25 23:38:07 -0500

    3°-3 Adición y pulido de información. =B

--- Js/programajs8.js
+++ Js/programajs8.js
@@ -1,24 +1,34 @@
-let mascota = {
+let perra = {
   especie: "perro",
   años: 2,
   nombre: "Galleta",
   feliz: true
 }
+let nickname = {
+  nickname: "peluda"
+}
 
-console.log("Mi perra se llama " + mascota["nombre"])
+console.log("Mi perra y mi objeto se llaman " + perra["nombre"]);
 
-let atributos = Object.values(mascota)
-console.log(atributos)
+let valores = Object.values(perra);
+console.log(".values: ");
+console.log(valores);
 
-let valores = Object.keys(mascota)
-console.log(valores)
+let atributos = Object.keys(perra);
+console.log(".keys: ");
+console.log(atributos);
 
-let nuse = Object.assign(mascota)
-console.log(nuse) //copia los datos de los objetos colocados en los paramentros a una variable asignada
+let atributosValores = Object.entries(perra);
+console.log(".entries: ");
+console.log(atributosValores);
 
-const dado = Math.random()*10
-const redond = Math.round(dado)
-console.log(`Número aleatorio: ${redond}`)
+let nuse = Object.assign(perra, nickname);
+console.log(".assign: ");
+console.log(nuse); //copia los datos de los objetos colocados en el parámentro a una variable asignada
 
-const dia = new Date()
-console.log("El día de hoy es " + dia.getUTCDate() + " del mes " + dia.getUTCMonth() + " del año " + dia.getUTCFullYear())
+const dado = Math.random()*10;
+const redond = Math.round(dado);
+console.log(`Número aleatorio: ${redond}`);
+
+const dia = new Date();
+console.log("El día de hoy es " + dia.getUTCDate() + " del mes " + dia.getUTCMonth() + " del año " + dia.getUTCFullYear());
